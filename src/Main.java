c1ecc55cfcda5cca21ab3645798fcf824aa3b191
VanjaG <vanjagodachii@gmail.com>
2020-11-11 21:03:06 +0200

    Commit 1

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b =   25,  b1 =  18;
+
+        System.out.println("Byte ");
+        System.out.println(" " + b + " - " + b1 + " = " + (b - b1));
+        System.out.println(" " + b + " + " + b1 + " = " + (b + b1));
+        System.out.println(" " + b + " * " + b1 + " = " + (b * b1));
+        System.out.println(" " + b + " / " + b1 + " = " + (b / b1));
+
+        System.out.println("Short ");
+        short sh = 15, sh1 = 35;
+        System.out.println(" " + sh + " - " + sh1 + " = " + (sh - sh1));
+        System.out.println(" " + sh + " + " + sh1 + " = " + (sh + sh1));
+        System.out.println(" " + sh + " * " + sh1 + " = " + (sh * sh1));
+        System.out.println(" " + sh + " / " + sh1 + " = " + (sh / sh1));
+
+        System.out.println("Long ");
+        long l = 145, l1 = 241;
+        System.out.println(" " + l + " - " + l1 + " = " + (l - l1));
+        System.out.println(" " + l + " + " + l1 + " = " + (l + l1));
+        System.out.println(" " + l + " * " + l1 + " = " + (l * l1));
+        System.out.println(" " + l + " / " + l1 + " = " + (l / l1));
+
+
+        System.out.println("Double ");
+        double d = 2.521, d1 = 5.353;
+        System.out.println(" " + d + " - " + d1 + " = " + (d - d1));
+        System.out.println(" " + d + " + " + d1 + " = " + (d + d1));
+        System.out.println(" " + d + " * " + d1 + " = " + (d * d1));
+        System.out.println(" " + d + " / " + d1 + " = " + (d / d1));
+
+        System.out.println("Float ");
+        float f = 10.1f, f1 = 46.2f;
+        System.out.println(" " + f + " - " + f1 + " = " + (f - f1));
+        System.out.println(" " + f + " + " + f1 + " = " + (f + f1));
+        System.out.println(" " + f + " * " + f1 + " = " + (f * f1));
+        System.out.println(" " + f + " / " + f1 + " = " + (f / f1));
+
+
+    }
+}
