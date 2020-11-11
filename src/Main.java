568fe02af73d0d0d8948585dbcfbc06ae66fbc57
VanjaG <vanjagodachii@gmail.com>
2020-11-11 22:49:28 +0200

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,41 @@
 public class Main {
-    public static void main(String[] args) {
-        byte b = 25, b1 = 18;
+        public static void main(String[] args) {
+            byte b = 25, b1 = 18;
 
-        System.out.println("Byte ");
-        System.out.println(" " + b + " + " + b1 + " = " + (b + b1));
-        System.out.println(" " + b + " - " + b1 + " = " + (b - b1));
-        System.out.println(" " + b + " * " + b1 + " = " + (b * b1));
-        System.out.println(" " + b + " / " + b1 + " = " + (b / b1));
+            System.out.println("Byte: ");
+            System.out.println("Addition: " + b + " + " + b1 + " = " + (b + b1));
+            System.out.println("Subtraction: " + b + " - " + b1 + " = " + (b - b1));
+            System.out.println("Multiplication: " + b + " * " + b1 + " = " + (b * b1));
+            System.out.println("Division: " + b + " / " + b1 + " = " + (b / b1));
 
-        System.out.println("Short ");
-        short sh = 15, sh1 = 35;
-        System.out.println(" " + sh + " + " + sh1 + " = " + (sh + sh1));
-        System.out.println(" " + sh + " - " + sh1 + " = " + (sh - sh1));
-        System.out.println(" " + sh + " * " + sh1 + " = " + (sh * sh1));
-        System.out.println(" " + sh + " / " + sh1 + " = " + (sh / sh1));
+            System.out.println("Short: ");
+            short sh = 15, sh1 = 35;
+            System.out.println("Addition: " + sh + " + " + sh1 + " = " + (sh + sh1));
+            System.out.println("Subtraction: " + sh + " - " + sh1 + " = " + (sh - sh1));
+            System.out.println("Multiplication: " + sh + " * " + sh1 + " = " + (sh * sh1));
+            System.out.println("Division: " + sh + " / " + sh1 + " = " + (sh / sh1));
 
-        System.out.println("Long ");
-        long l = 145, l1 = 241;
-        System.out.println(" " + l + " + " + l1 + " = " + (l + l1));
-        System.out.println(" " + l + " - " + l1 + " = " + (l - l1));
-        System.out.println(" " + l + " * " + l1 + " = " + (l * l1));
-        System.out.println(" " + l + " / " + l1 + " = " + (l / l1));
+            System.out.println("Long: ");
+            long l = 145, l1 = 241;
+            System.out.println("Addition: " + l + " + " + l1 + " = " + (l + l1));
+            System.out.println("Subtraction: " + l + " - " + l1 + " = " + (l - l1));
+            System.out.println("Multiplication: " + l + " * " + l1 + " = " + (l * l1));
+            System.out.println("Division: " + l + " / " + l1 + " = " + (l / l1));
 
 
-        System.out.println("Double ");
-        double d = 2.521, d1 = 5.353;
-        System.out.println(" " + d + " + " + d1 + " = " + (d + d1));
-        System.out.println(" " + d + " - " + d1 + " = " + (d - d1));
-        System.out.println(" " + d + " * " + d1 + " = " + (d * d1));
-        System.out.println(" " + d + " / " + d1 + " = " + (d / d1));
+            System.out.println("Double: ");
+            double d = 2.521, d1 = 5.353;
+            System.out.println("Addition: " + d + " + " + d1 + " = " + (d + d1));
+            System.out.println("Subtraction: " + d + " - " + d1 + " = " + (d - d1));
+            System.out.println("Multiplication: " + d + " * " + d1 + " = " + (d * d1));
+            System.out.println("Division: " + d + " / " + d1 + " = " + (d / d1));
 
-        System.out.println("Float ");
-        float f = 10.1f, f1 = 46.2f;
-        System.out.println(" " + f + " + " + f1 + " = " + (f + f1));
-        System.out.println(" " + f + " - " + f1 + " = " + (f - f1));
-        System.out.println(" " + f + " * " + f1 + " = " + (f * f1));
-        System.out.println(" " + f + " / " + f1 + " = " + (f / f1));
+            System.out.println("Float: ");
+            float f = 10.1f, f1 = 46.2f;
+            System.out.println("Addition: " + f + " + " + f1 + " = " + (f + f1));
+            System.out.println("Subtraction: " + f + " - " + f1 + " = " + (f - f1));
+            System.out.println("Multiplication: " + f + " * " + f1 + " = " + (f * f1));
+            System.out.println("Division: " + f + " / " + f1 + " = " + (f / f1));
 
-    }
+        }
 }
